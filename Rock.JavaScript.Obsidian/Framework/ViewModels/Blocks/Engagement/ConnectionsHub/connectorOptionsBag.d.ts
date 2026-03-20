1d3c44aedb005eeb08627c9e38d218ef0d269257
kylehenning <kylebhenning@gmail.com>
2026-03-20 11:10:21 -0700

    - Added Campus logic on add and edit features inside the Connections List View. Fixed various QA kickback bugs.

--- Rock.JavaScript.Obsidian/Framework/ViewModels/Blocks/Engagement/ConnectionsHub/connectorOptionsBag.d.ts
+++ Rock.JavaScript.Obsidian/Framework/ViewModels/Blocks/Engagement/ConnectionsHub/connectorOptionsBag.d.ts
@@ -21,24 +21,13 @@
 // </copyright>
 //
 
-import { ConnectorOptionsBag } from "@Obsidian/ViewModels/Blocks/Engagement/ConnectionsHub/connectorOptionsBag";
 import { ListItemBag } from "@Obsidian/ViewModels/Utility/listItemBag";
-import { PublicAttributeBag } from "@Obsidian/ViewModels/Utility/publicAttributeBag";
 
 /** Represents the detail data for a connection opportunity that is used to populate the request panel when an opportunity is selected. */
-export type ConnectionOpportunityDetailBag = {
-    /** Gets or sets the list of available campuses for this opportunity. */
-    campuses?: ListItemBag[] | null;
+export type ConnectorOptionsBag = {
+    /** Gets or sets the list of connectors available for assignment within this opportunity. */
+    connectors?: ListItemBag[] | null;
 
-    /** Gets or sets the attributes for Connection Request attributes specified at the Connection Opportunity level. */
-    connectionOpportunityRequestAttributes?: Record<string, PublicAttributeBag> | null;
-
-    /** Gets or sets the connector options. */
-    connectorOptions?: ConnectorOptionsBag | null;
-
-    /** Gets or sets the encrypted identifier key of this connection opportunity. */
-    idKey?: string | null;
-
-    /** Gets or sets the list of placement groups available for this opportunity. */
-    placementGroups?: ListItemBag[] | null;
+    /** Gets or sets the GUID of the default connector's person alias for this opportunity. */
+    defaultConnectorPersonAliasGuid?: string | null;
 };
